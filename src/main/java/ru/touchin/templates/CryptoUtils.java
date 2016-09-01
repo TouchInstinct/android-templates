ee274dc28fce91ce39b84b86faca3e792aaba7d9
Gavriil Sitnikov <zuzuk666@gmail.com>
2016-09-01 03:24:38 +0300

    CryptoUtils and Sockets in release state candidate

--- src/main/java/ru/touchin/templates/CryptoUtils.java
+++ src/main/java/ru/touchin/templates/CryptoUtils.java
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (c) 2016 Touch Instinct
+ *
+ *  This file is part of RoboSwag library.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package ru.touchin.templates;
 
 import android.support.annotation.NonNull;
@@ -9,22 +28,24 @@ import java.security.GeneralSecurityException;
 
 /**
  * Created by Gavriil Sitnikov on 30/08/2016.
- * TODO: fill description
+ * Utility class that is providing common methods related to cryptography.
  */
 public final class CryptoUtils {
 
     /**
+     * Just encrypts bytes by key in good way.
+     * To decrypt them use {@link #simpleDecryptBytes(byte[], String)}.
      * Dependency needed: compile 'com.scottyab:aes-crypto:+'.
      *
-     * @param bytes
-     * @param keyString
-     * @return
+     * @param bytesToDecrypt Bytes to encrypt;
+     * @param keyString      Encryption key;
+     * @return Encrypted bytes.
      */
     @NonNull
-    public static byte[] simpleEncryptBytes(@NonNull final byte[] bytes, @NonNull final String keyString) {
+    public static byte[] simpleEncryptBytes(@NonNull final byte[] bytesToDecrypt, @NonNull final String keyString) {
         try {
             final AesCbcWithIntegrity.SecretKeys key = AesCbcWithIntegrity.keys(keyString);
-            final AesCbcWithIntegrity.CipherTextIvMac cipherTextIvMac = AesCbcWithIntegrity.encrypt(bytes, key);
+            final AesCbcWithIntegrity.CipherTextIvMac cipherTextIvMac = AesCbcWithIntegrity.encrypt(bytesToDecrypt, key);
             return cipherTextIvMac.toString().getBytes(Charset.forName("UTF-8"));
         } catch (final GeneralSecurityException exception) {
             throw new RuntimeException(exception);
@@ -32,18 +53,19 @@ public final class CryptoUtils {
     }
 
     /**
+     * Just decrypts bytes which are encrypted by {@link #simpleEncryptBytes(byte[], String)}.
      * Dependency needed: compile 'com.scottyab:aes-crypto:+'.
      *
-     * @param encrypted
-     * @param keyString
-     * @return
+     * @param encryptedBytes Bytes to decrypt;
+     * @param keyString      Encryption key;
+     * @return Encrypted bytes.
      */
     @NonNull
-    public static byte[] simpleDecryptBytes(@NonNull final byte[] encrypted, @NonNull final String keyString) {
+    public static byte[] simpleDecryptBytes(@NonNull final byte[] encryptedBytes, @NonNull final String keyString) {
         try {
             final AesCbcWithIntegrity.SecretKeys key = AesCbcWithIntegrity.keys(keyString);
             final AesCbcWithIntegrity.CipherTextIvMac cipherTextIvMac
-                    = new AesCbcWithIntegrity.CipherTextIvMac(new String(encrypted, Charset.forName("UTF-8")));
+                    = new AesCbcWithIntegrity.CipherTextIvMac(new String(encryptedBytes, Charset.forName("UTF-8")));
             return AesCbcWithIntegrity.decrypt(cipherTextIvMac, key);
         } catch (final GeneralSecurityException exception) {
             throw new RuntimeException(exception);
