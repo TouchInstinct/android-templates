33903efadc9026cd9e33c9dcd27b451895af8875
Denis Karmyshakov <Denis.Karmyshakov@gmail.com>
2018-05-25 21:09:47 +0300

    dispatchTo methods now returns Disposable

--- src/main/java/ru/touchin/templates/livedata/BaseLiveDataDispatcher.kt
+++ src/main/java/ru/touchin/templates/livedata/BaseLiveDataDispatcher.kt
@@ -6,6 +6,7 @@ import io.reactivex.Flowable
 import io.reactivex.Maybe
 import io.reactivex.Observable
 import io.reactivex.Single
+import io.reactivex.disposables.Disposable
 import ru.touchin.roboswag.components.utils.destroyable.Destroyable
 import ru.touchin.templates.livedata.event.CompletableEvent
 import ru.touchin.templates.livedata.event.MaybeEvent
@@ -14,39 +15,39 @@ import ru.touchin.templates.livedata.event.SingleEvent
 
 class BaseLiveDataDispatcher(private val destroyable: Destroyable) : LiveDataDispatcher {
 
-    override fun <T> Flowable<T>.dispatchTo(liveData: MutableLiveData<ObservableEvent<T>>) {
+    override fun <T> Flowable<T>.dispatchTo(liveData: MutableLiveData<ObservableEvent<T>>): Disposable {
         liveData.value = ObservableEvent.Loading(liveData.value?.data)
-        destroyable.untilDestroy(this,
+        return destroyable.untilDestroy(this,
                 { data -> liveData.value = ObservableEvent.Success(data) },
                 { throwable -> liveData.value = ObservableEvent.Error(throwable, liveData.value?.data) },
                 { liveData.value = ObservableEvent.Completed(liveData.value?.data) })
     }
 
-    override fun <T> Observable<T>.dispatchTo(liveData: MutableLiveData<ObservableEvent<T>>) {
+    override fun <T> Observable<T>.dispatchTo(liveData: MutableLiveData<ObservableEvent<T>>): Disposable {
         liveData.value = ObservableEvent.Loading(liveData.value?.data)
-        destroyable.untilDestroy(this,
+        return destroyable.untilDestroy(this,
                 { data -> liveData.value = ObservableEvent.Success(data) },
                 { throwable -> liveData.value = ObservableEvent.Error(throwable, liveData.value?.data) },
                 { liveData.value = ObservableEvent.Completed(liveData.value?.data) })
     }
 
-    override fun <T> Single<T>.dispatchTo(liveData: MutableLiveData<SingleEvent<T>>) {
+    override fun <T> Single<T>.dispatchTo(liveData: MutableLiveData<SingleEvent<T>>): Disposable {
         liveData.value = SingleEvent.Loading(liveData.value?.data)
-        destroyable.untilDestroy(this,
+        return destroyable.untilDestroy(this,
                 { data -> liveData.value = SingleEvent.Success(data) },
                 { throwable -> liveData.value = SingleEvent.Error(throwable, liveData.value?.data) })
     }
 
-    override fun Completable.dispatchTo(liveData: MutableLiveData<CompletableEvent>) {
+    override fun Completable.dispatchTo(liveData: MutableLiveData<CompletableEvent>): Disposable {
         liveData.value = CompletableEvent.Loading
-        destroyable.untilDestroy(this,
+        return destroyable.untilDestroy(this,
                 { liveData.value = CompletableEvent.Completed },
                 { throwable -> liveData.value = CompletableEvent.Error(throwable) })
     }
 
-    override fun <T> Maybe<T>.dispatchTo(liveData: MutableLiveData<MaybeEvent<T>>) {
+    override fun <T> Maybe<T>.dispatchTo(liveData: MutableLiveData<MaybeEvent<T>>): Disposable {
         liveData.value = MaybeEvent.Loading(liveData.value?.data)
-        destroyable.untilDestroy(this,
+        return destroyable.untilDestroy(this,
                 { data -> liveData.value = MaybeEvent.Success(data) },
                 { throwable -> liveData.value = MaybeEvent.Error(throwable, liveData.value?.data) },
                 { liveData.value = MaybeEvent.Completed(liveData.value?.data) })
--- src/main/java/ru/touchin/templates/livedata/LiveDataDispatcher.kt
+++ src/main/java/ru/touchin/templates/livedata/LiveDataDispatcher.kt
@@ -6,6 +6,7 @@ import io.reactivex.Flowable
 import io.reactivex.Maybe
 import io.reactivex.Observable
 import io.reactivex.Single
+import io.reactivex.disposables.Disposable
 import ru.touchin.templates.livedata.event.CompletableEvent
 import ru.touchin.templates.livedata.event.MaybeEvent
 import ru.touchin.templates.livedata.event.ObservableEvent
@@ -13,14 +14,14 @@ import ru.touchin.templates.livedata.event.SingleEvent
 
 interface LiveDataDispatcher {
 
-    fun <T> Flowable<T>.dispatchTo(liveData: MutableLiveData<ObservableEvent<T>>)
+    fun <T> Flowable<T>.dispatchTo(liveData: MutableLiveData<ObservableEvent<T>>): Disposable
 
-    fun <T> Observable<T>.dispatchTo(liveData: MutableLiveData<ObservableEvent<T>>)
+    fun <T> Observable<T>.dispatchTo(liveData: MutableLiveData<ObservableEvent<T>>): Disposable
 
-    fun <T> Single<T>.dispatchTo(liveData: MutableLiveData<SingleEvent<T>>)
+    fun <T> Single<T>.dispatchTo(liveData: MutableLiveData<SingleEvent<T>>): Disposable
 
-    fun Completable.dispatchTo(liveData: MutableLiveData<CompletableEvent>)
+    fun Completable.dispatchTo(liveData: MutableLiveData<CompletableEvent>): Disposable
 
-    fun <T> Maybe<T>.dispatchTo(liveData: MutableLiveData<MaybeEvent<T>>)
+    fun <T> Maybe<T>.dispatchTo(liveData: MutableLiveData<MaybeEvent<T>>): Disposable
 
 }
